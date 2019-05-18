c535f83d25c1ad0417d6597141b39407d8a47dd9
Rafael Prallon <rafaelprallon@gmail.com>
2019-05-17 21:33:34 -0300

    adicionando o chat channel

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'chats/show'
+
+  root to: 'chats#show'
+
+  mount ActionCable.server => '/cable'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
